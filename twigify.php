611aedcaebb1797b6c451f3ae3ab83b3f3f49354
Mike Van Winkle <mike@mikevanwinkle.com>
2014-12-31 00:04:55 +0000

    Backwards compatibility and update travis reqs, not going to support php 5.3

--- twigify.php
+++ twigify.php
@@ -57,7 +57,7 @@ class ContentTemplatesPlugin {
 	}
 
 	static function activate() {
-		if ( self::settings() ) {
+		if ( ContentTemplatesPlugin::settings() ) {
 		}
 	}
 
@@ -134,7 +134,7 @@ class ContentTemplatesPlugin {
 	}
 
 	static function settings() {
-		$plugin = self::instance();
+		$plugin = ContentTemplatesPlugin::instance();
 		return $plugin->get_settings();
 	}
 
@@ -158,7 +158,7 @@ class ContentTemplatesPlugin {
 		$templates = new WP_Query(array('post_type'=>'ctemplates'));
 		$views = \ContentTemplates\View::instance();
 		$data = array();
-		$data['settings'] = self::settings();
+		$data['settings'] = ContentTemplatesPlugin::settings();
 		if (!isset($data['settings']['roles'])) {
 			$data['settings']['roles'] = array();
 		}
@@ -174,7 +174,7 @@ class ContentTemplatesPlugin {
 
 	static function instance() {
 		if ( !self::$instance ) {
-			self::$instance = new Self();
+			self::$instance = new ContentTemplatesPlugin();
 		}
 		return self::$instance;
 	}
