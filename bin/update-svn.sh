651f7c2082f6afb07113c79cbcb965c2866e036d
root <root@vvv>
2015-01-01 01:52:44 +0000

    Script to commit svn after successful CI run

---
+++ bin/update-svn.sh
@@ -0,0 +1,133 @@
+#! /bin/bash
+
+PLUGINSLUG='twigify'
+
+# Set up some default values. Feel free to change these in your own script
+CURRENTDIR=`pwd`
+echo "CURRENT is $CURRENTDIR"
+SVNPATH="/tmp/$PLUGINSLUG"
+SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG"
+SVNUSER="mpvanwinkle77"
+PLUGINDIR="$CURRENTDIR/$PLUGINSLUG"
+MAILFILE="$PLUGINSLUG.php"
+
+# git config
+GITPATH="$PLUGINDIR/" # this file should be in the base of your git repository
+
+# Let's begin...
+echo ".........................................."
+echo 
+echo "Preparing to deploy WordPress plugin"
+echo 
+echo ".........................................."
+echo 
+
+# Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
+NEWVERSION1=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}' | tr -d '\r'`
+echo "readme.txt version: $NEWVERSION1"
+NEWVERSION2=`grep "Version:" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}' | tr -d '\r'`
+echo "$MAINFILE version: $NEWVERSION2"
+
+if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Version in readme.txt & $MAINFILE don't match. Exiting...."; exit 1; fi
+
+echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
+
+# GaryJ: Ignore check for git tag, as git flow release finish creates this.
+#if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
+#	then 
+#		echo "Version $NEWVERSION1 already exists as git tag. Exiting...."; 
+#		exit 1; 
+#	else
+#		echo "Git version does not exist. Let's proceed..."
+#fi
+
+echo "Changing to $GITPATH"
+cd $GITPATH
+# GaryJ: Commit message variable not needed . Hard coded for SVN trunk commit for consistency.
+#echo -e "Enter a commit message for this new version: \c"
+#read COMMITMSG
+# GaryJ: git flow release finish already covers this commit.
+#git commit -am "$COMMITMSG"
+
+# GaryJ: git flow release finish already covers this tag creation.
+#echo "Tagging new version in git"
+#git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+
+echo "Pushing git master to origin, with tags"
+git push origin master
+git push origin master --tags
+
+echo 
+echo "Creating local copy of SVN repo trunk ..."
+svn checkout $SVNURL $SVNPATH --depth immediates
+svn update --quiet $SVNPATH/trunk --set-depth infinity
+
+echo "Ignoring GitHub specific files"
+svn propset svn:ignore "README.md
+Thumbs.db
+.git
+.gitignore" "$SVNPATH/trunk/"
+
+echo "Exporting the HEAD of master from git to the trunk of SVN"
+git checkout-index -a -f --prefix=$SVNPATH/trunk/
+
+# If submodule exist, recursively check out their indexes
+if [ -f ".gitmodules" ]
+	then
+		echo "Exporting the HEAD of each submodule from git to the trunk of SVN"
+		git submodule init
+		git submodule update
+		git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+			while read path_key path
+			do
+				#url_key=$(echo $path_key | sed 's/\.path/.url/')
+				#url=$(git config -f .gitmodules --get "$url_key")
+				#git submodule add $url $path
+				echo "This is the submodule path: $path"
+				echo "The following line is the command to checkout the submodule."
+				echo "git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'"
+				git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'
+			done
+fi
+
+# Support for the /assets folder on the .org repo.
+echo "Moving assets"
+# Make the directory if it doesn't already exist
+mkdir -p $SVNPATH/assets/
+mv $SVNPATH/trunk/assets/* $SVNPATH/assets/
+svn add --force $SVNPATH/assets/
+svn delete --force $SVNPATH/trunk/assets
+
+echo "Changing directory to SVN and committing to trunk"
+cd $SVNPATH/trunk/
+# Delete all files that should not now be added.
+svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2}' | xargs svn del
+# Add all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+svn commit --username=$SVNUSER -m "Preparing for $NEWVERSION1 release"
+
+echo "Updating WordPress plugin repo assets and committing"
+cd $SVNPATH/assets/
+# Delete all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2}' | xargs svn del
+# Add all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+svn update --accept mine-full $SVNPATH/assets/*
+svn commit --username=$SVNUSER -m "Updating assets"
+
+echo "Creating new SVN tag and committing it"
+cd $SVNPATH
+svn update --quiet $SVNPATH/tags/$NEWVERSION1
+svn copy --quiet trunk/ tags/$NEWVERSION1/
+# Remove assets and trunk directories from tag directory
+svn delete --force --quiet $SVNPATH/tags/$NEWVERSION1/assets
+svn delete --force --quiet $SVNPATH/tags/$NEWVERSION1/trunk
+cd $SVNPATH/tags/$NEWVERSION1
+svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
+
+echo "Removing temporary directory $SVNPATH"
+cd $SVNPATH
+cd ..
+rm -fr $SVNPATH/
+
+echo "*** FIN ***"
